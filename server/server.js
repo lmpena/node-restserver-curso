fcad6b1c51766bb90a3cc37a045cf0dc69a51152
Luis Peña <lmpenam69@gmail.com>
2020-08-07 13:09:58 +0200

    config puerto

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -39,6 +41,6 @@ app.post('/usuario', (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando puerto: ', 3000);
-})
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
